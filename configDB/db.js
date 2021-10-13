3048f883c1f567ce17c8c1c9ce06711e8b912b68
aymanyaich <aymenyaiche@yahoo.fr>
2021-10-13 15:09:45 +0100

    lll

--- configDB/db.js
+++ configDB/db.js
@@ -2,12 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI,{
-      useNewUrlParser: true,
-      useFindAndModify: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-    });
+    await mongoose.connect(process.env.MONGO_URI,);
      
 
     console.log("MongoDB Connection Success 👍");
