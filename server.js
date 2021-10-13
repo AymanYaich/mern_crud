4bf63b3f1b4b6441f9b2ef54af74d2e3e0358bc5
aymanyaich <aymenyaiche@yahoo.fr>
2021-10-13 16:23:57 +0100

    88

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ connectDb();
 app.use('/custumer',custumer)
 
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname,"/client/build")))
+  app.use(express.static(path.join(__dirname,"client","build")))
   app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,"client","build","index.html"))
   })
